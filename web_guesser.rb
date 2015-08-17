78a9f6a4299205457e7e7c7f5448ef80444b9240
David Stinnette <dastinnette@gmail.com>
2015-08-17 15:37:35 -0600

    Add too low! and just right! functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,13 +4,17 @@ require 'sinatra/reloader'
   NUM = rand(100)
 
   def check_guess(guess)
-    if guess.to_i > NUM
+    if guess == NUM
+      "You got it right! The SECRET NUMBER is #{NUM}"
+    elsif guess > NUM
       "Too high!"
+    elsif guess < NUM
+      "Too low!"
     end
   end
 
   get '/' do
-    guess   = params["guess"]
+    guess   = params["guess"].to_i
     message = check_guess(guess)
     erb :index, :locals => {:number => NUM, :message => message}
   end

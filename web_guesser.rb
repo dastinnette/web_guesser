51bdaa9852861753d6cedb01c910865b18cd0f6d
David Stinnette <dastinnette@gmail.com>
2015-08-17 15:49:10 -0600

    Add way too high/low functionality

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,14 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-  NUM = rand(100)
+  SECRET_NUM = rand(100)
 
   def check_guess(guess)
-    if guess == NUM
-      "You got it right! The SECRET NUMBER is #{NUM}"
-    elsif guess > NUM
+    if guess == SECRET_NUM
+      "You got it right! The SECRET NUMBER is #{SECRET_NUM}"
+    elsif (guess - 5) > SECRET_NUM
+      "Way too high!"
+    elsif (guess + 5) < SECRET_NUM
+      "Way too low!"
+    elsif guess > SECRET_NUM
       "Too high!"
-    elsif guess < NUM
+    elsif guess < SECRET_NUM
       "Too low!"
     end
   end
@@ -16,5 +20,5 @@ require 'sinatra/reloader'
   get '/' do
     guess   = params["guess"].to_i
     message = check_guess(guess)
-    erb :index, :locals => {:number => NUM, :message => message}
+    erb :index, :locals => {:number => SECRET_NUM, :message => message}
   end
